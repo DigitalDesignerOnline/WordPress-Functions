be464684976041586f033b7a85ae570184e55ed3
DigitalDesigner <info@digitaldesigneronline.com>
2017-10-05 00:51:00 -0400

    Update Dashboard Admin footer text
    Add this code snippet to your functions.php file of your desired WordPress theme to customize the WordPress dashboard admin area with your own custom message. Great for client sites..

--- dashboard-footer.php
+++ dashboard-footer.php
@@ -1,6 +1,6 @@
 function update_dashboard_footer () 
 {
-    echo '<span id="footer-thankyou">Development &amp; Customization Provided By: <a href="http://www.redsealcreative.com" target="_blank">RedSeal Creative Inc.</a></span>';
+    echo '<span id="footer-thankyou">Development &amp; Customization Provided By: <a href="http://www.yourdomain.com" target="_blank">Your Company Name</a></span>';
 }
  
 add_filter('admin_footer_text', 'update_dashboard_footer');
